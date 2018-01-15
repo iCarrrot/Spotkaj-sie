bc640cea19bdd0e2bb327685ccc8ddaa8a2d4ea0
iCarrrot <michal.mar3@gmail.com>
2018-01-15 15:48:54 +0100

    .

--- spotkaj-sie/polls/admin.py
+++ spotkaj-sie/polls/admin.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+    verbose_name = 'Odpowiedzi'
+
+
+class BaseQuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']})
+    ]
+
+    list_display = ('question_text', )
+    verbose_name = 'Pytanie'
+
+
+class QuestionAdmin(BaseQuestionAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
--- spotkaj-sie/polls/models.py
+++ spotkaj-sie/polls/models.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from datetime import date
 from django.db import models
-
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,13 +11,18 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    """
+    Class representing answer to a question.
+    """
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField('Odpowiedź', max_length=200)
+    votes = models.IntegerField('Liczba głosów', default=0)
 
     def __str__(self):
         return self.choice_text
